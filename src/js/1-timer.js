e6a95c9426dd9f6a7eb220dff1160633bb84c898
Olesya Lobodko <olesya.lobodko@gmail.com>
2024-04-22 01:01:51 +0200

    installed izitoast library

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,7 @@
-// Описаний в документації
 import flatpickr from 'flatpickr';
-// Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const options = {
   enableTime: true,
